ce04f88a2ed47bebd91d978fd3540b9169abe1f0
Hunrin <t84sta@gmail.com>
2019-07-21 20:39:36 +0200

    Updates

---
+++ precache-manifest.e5987fd6adaf25fc30b081f2f135f8b8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d685656e79304656668c4f1aa6dbd34",
+    "url": "/memory-game/static/media/3.7d685656.png"
+  },
+  {
+    "revision": "9a49b7d61ecf22582c4e",
+    "url": "/memory-game/static/css/main.8af47d2e.chunk.css"
+  },
+  {
+    "revision": "435a81855f49d55acd25",
+    "url": "/memory-game/static/js/runtime~main.435a8185.js"
+  },
+  {
+    "revision": "70ba05ea4df4196de6e8",
+    "url": "/memory-game/static/js/2.70ba05ea.chunk.js"
+  },
+  {
+    "revision": "059715b3be90463ffd26bd18141e2479",
+    "url": "/memory-game/static/media/dawn.059715b3.jpg"
+  },
+  {
+    "revision": "118c2106137e6fa5488e7340189afc4d",
+    "url": "/memory-game/static/media/10.118c2106.png"
+  },
+  {
+    "revision": "9a49b7d61ecf22582c4e",
+    "url": "/memory-game/static/js/main.9a49b7d6.chunk.js"
+  },
+  {
+    "revision": "1d04cedce08abe23ee3887cb91fce0cd",
+    "url": "/memory-game/static/media/5.1d04cedc.png"
+  },
+  {
+    "revision": "b2d0b70abfba23e747615e0af3ecf40d",
+    "url": "/memory-game/static/media/6.b2d0b70a.png"
+  },
+  {
+    "revision": "ad8b44bdd37e90ef95d9695ca9e38c4f",
+    "url": "/memory-game/static/media/7.ad8b44bd.png"
+  },
+  {
+    "revision": "ee665fe67e5f6bced2dd6ea7d17a9d41",
+    "url": "/memory-game/static/media/9.ee665fe6.png"
+  },
+  {
+    "revision": "ab492f5271d8b9e4d5d8eac754ad0aaa",
+    "url": "/memory-game/static/media/back.ab492f52.png"
+  },
+  {
+    "revision": "7a7bb5cee52549fd35de41642de95ec5",
+    "url": "/memory-game/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6bbb297b5bc2391154301ec741d1ed9.js"
+  "/memory-game/precache-manifest.e5987fd6adaf25fc30b081f2f135f8b8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/memory-game/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
