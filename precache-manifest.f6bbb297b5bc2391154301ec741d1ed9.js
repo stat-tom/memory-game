bb1361cd2a40336bc34e43b805af94871b35fd2c
Hunrin <t84sta@gmail.com>
2019-07-21 18:39:07 +0200

    Updates

---
+++ precache-manifest.f6bbb297b5bc2391154301ec741d1ed9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d685656e79304656668c4f1aa6dbd34",
+    "url": "/static/media/3.7d685656.png"
+  },
+  {
+    "revision": "48f70eae65d965a966a3",
+    "url": "/static/css/main.9b338c3c.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "70ba05ea4df4196de6e8",
+    "url": "/static/js/2.70ba05ea.chunk.js"
+  },
+  {
+    "revision": "059715b3be90463ffd26bd18141e2479",
+    "url": "/static/media/dawn.059715b3.jpg"
+  },
+  {
+    "revision": "118c2106137e6fa5488e7340189afc4d",
+    "url": "/static/media/10.118c2106.png"
+  },
+  {
+    "revision": "48f70eae65d965a966a3",
+    "url": "/static/js/main.48f70eae.chunk.js"
+  },
+  {
+    "revision": "1d04cedce08abe23ee3887cb91fce0cd",
+    "url": "/static/media/5.1d04cedc.png"
+  },
+  {
+    "revision": "b2d0b70abfba23e747615e0af3ecf40d",
+    "url": "/static/media/6.b2d0b70a.png"
+  },
+  {
+    "revision": "ad8b44bdd37e90ef95d9695ca9e38c4f",
+    "url": "/static/media/7.ad8b44bd.png"
+  },
+  {
+    "revision": "ee665fe67e5f6bced2dd6ea7d17a9d41",
+    "url": "/static/media/9.ee665fe6.png"
+  },
+  {
+    "revision": "ab492f5271d8b9e4d5d8eac754ad0aaa",
+    "url": "/static/media/back.ab492f52.png"
+  },
+  {
+    "revision": "4d32ae40a660709e36d38e99b070ab19",
+    "url": "/index.html"
+  }
+];
